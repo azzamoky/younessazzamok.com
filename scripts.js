49e00fbbaeb020140d8e20043c379435d2ca79e0
azzamoky <youness.azzamok@icloud.com>
2024-07-18 22:53:45 -0400

    Update and rename scripts.js to h

--- scripts.js
+++
@@ -1,9 +0,0 @@
-// scripts.js
-document.addEventListener('DOMContentLoaded', function () {
-    const menuIcon = document.getElementById('menu-icon');
-    const menu = document.getElementById('menu');
-
-    menuIcon.addEventListener('click', function () {
-        menu.classList.toggle('show');
-    });
-});
